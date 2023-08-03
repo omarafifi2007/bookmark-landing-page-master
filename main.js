6095c29e095a76fb56411595cb00d6babded2396
omarafifi2007 <omarafifi289@gmail.com>
2023-08-03 09:16:53 +0300

    bookmark-landing-page-master

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById("Log").onclick = function ()
+{
+    document.getElementById("Log").style.backgroundColor = 'red';
+    document.getElementById("Log").style.color = 'white';
+}
+document.getElementById("BTN1").onclick = function ()
+{
+    document.getElementById("BTN1").style.backgroundColor = 'hsl(231, 69%, 60%)';
+}
+document.getElementById("BTN2").onclick = function ()
+{
+    document.getElementById("BTN2").style.backgroundColor = 'hsl(231, 69%, 60%)';
+}
+document.getElementById("BTN3").onclick = function ()
+{
+    document.getElementById("BTN3").style.backgroundColor = 'hsl(231, 69%, 60%)';
+}
